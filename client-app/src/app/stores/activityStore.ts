2d61d0171b97ae24632a99a56f4375eac408c04d
Carlito Pardales <carlpardales@gmail.com>
2020-10-14 17:24:00 +1300

    Add styling to components Activity list Activity list item Activity details Activity form Home page

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -28,10 +28,20 @@ class ActivityStore {
   };
 
   get activitiesByDate() {
-    return Array.from(this.activityRegistry.values()).sort(
+    return this.groupActivitiesBydate(Array.from(this.activityRegistry.values()));
+  };
+
+  groupActivitiesBydate(activities: IActivity[]) {
+    const sortedActivities = activities.sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
     );
-  };
+
+    return Object.entries(sortedActivities.reduce((activities, activity) => {
+      const date = activity.date.split('T')[0];
+      activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+      return activities;
+    }, {} as {[key: string]: IActivity[]}));
+  }
 
   loadActivities = async () => {
     this.loadingInitial = true;
