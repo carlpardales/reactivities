7a2a08d7fd5e340806c8dd73218274d4d35c791f
Carlito Pardales <carlpardales@gmail.com>
2020-11-03 14:49:24 +1300

    Add home page and navbar user info

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Button, Container, Menu } from 'semantic-ui-react';
+import React, { useContext } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { Button, Container, Dropdown, Menu, Image } from 'semantic-ui-react';
+import { RootStoreContext } from '../../app/stores/rootStore';
 
 const NavBar: React.FC = () => {
+	const rootStore = useContext(RootStoreContext);
+	const { isLoggedIn, user } = rootStore.userStore;
+
 	return (
 		<Menu fixed='top' inverted>
 			<Container>
@@ -18,6 +22,21 @@ const NavBar: React.FC = () => {
 						content='Create Activity'
 					/>
 				</Menu.Item>
+				{user &&
+					<Menu.Item position='right'>
+						<Image avatar spaced='right' src={user.image || '/assets/user.png'} />
+						<Dropdown pointing='top left' text={user.displayName}>
+							<Dropdown.Menu>
+								<Dropdown.Item
+									as={Link} to={`/profile/username`}
+									text='My profile'
+									icon='user'
+								/>
+								<Dropdown.Item text='Logout' icon='power' />
+							</Dropdown.Menu>
+						</Dropdown>
+					</Menu.Item>
+				}
 			</Container>
 		</Menu>
 	)
