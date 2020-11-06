218362d7cbbbfc66702efc508dd96ccb4aa053bc
Carlito Pardales <carlpardales@gmail.com>
2020-11-06 14:50:19 +1300

    Add DTOs to shape data

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,22 +1,25 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Errors;
 using Domain;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
 {
     public class Details
     {
-        public class Query : IRequest<Activity>
+        public class Query : IRequest<ActivityDto>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, Activity>
+        public class Handler : IRequestHandler<Query, ActivityDto>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -24,15 +27,41 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<ActivityDto> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activity = await _context.Activities.FindAsync(request.Id);
+                var activity = await _context.Activities
+                    .Include(x => x.UserActivities)
+                    .ThenInclude(x => x.AppUser)
+                    .SingleOrDefaultAsync(x => x.Id == request.Id);
 
                 if (activity == null)
                     throw new RestException(HttpStatusCode.NotFound, new {
                         activity = "Not Found"});
 
-                return activity;
+                
+                var attendees = activity.UserActivities.Select( a => UserActivityToAttendeeDto(a)).ToList();
+                return new ActivityDto
+                {
+                    Id = activity.Id,
+                    Title = activity.Title,
+                    Description = activity.Description,
+                    Category = activity.Category,
+                    Date = activity.Date,
+                    City = activity.City,
+                    Venue = activity.Venue,
+                    Attendees = attendees
+                };
+            }
+
+            private AttendeeDto UserActivityToAttendeeDto(UserActivity userActivity) 
+            {
+                return new AttendeeDto
+                {
+                    Username = userActivity.AppUser.UserName,
+                    DisplayName = userActivity.AppUser.DisplayName,
+                    Image = null,
+                    IsHost = userActivity.IsHost
+                };
             }
         }
     }
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain;
@@ -10,8 +11,8 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>> { }
-        public class Handler : IRequestHandler<Query, List<Activity>>
+        public class Query : IRequest<List<ActivityDto>> { }
+        public class Handler : IRequestHandler<Query, List<ActivityDto>>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -19,11 +20,45 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<List<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activities = await _context.Activities.ToListAsync();
+                var activities = await _context.Activities
+                    .Include(x => x.UserActivities)
+                    .ThenInclude(x => x.AppUser)
+                    .ToListAsync();
 
-                return activities;
+                return activities.Select( a => ActivityToActivityDto(a))
+                                 .ToList();
+            }
+
+            private ActivityDto ActivityToActivityDto(Activity activity) 
+            {
+                var attendees = activity.UserActivities.Select( a => UserActivityToAttendeeDto(a)).ToList();
+
+                var activityDto = new ActivityDto
+                {
+                    Id = activity.Id,
+                    Title = activity.Title,
+                    Description = activity.Description,
+                    Category = activity.Category,
+                    Date = activity.Date,
+                    City = activity.City,
+                    Venue = activity.Venue,
+                    Attendees = attendees
+                };
+
+                return activityDto;
+            }
+
+            private AttendeeDto UserActivityToAttendeeDto(UserActivity userActivity) 
+            {
+                return new AttendeeDto
+                {
+                    Username = userActivity.AppUser.UserName,
+                    DisplayName = userActivity.AppUser.DisplayName,
+                    Image = null,
+                    IsHost = userActivity.IsHost
+                };
             }
         }
     }
